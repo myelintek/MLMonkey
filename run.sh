a1564fe0460d84411339e5a8754e097995f44096
root <root@localhost.localdomain>
2019-05-03 10:07:57 +0000

    use distributed training for COCO

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
 cd /workspace
 mkdir -p mlperf
 mkdir -p logs
-cd mlperf
-if [ ! -d "/workspace/mlperf/training" ]; then
+cd /run_benchmarks/benchmarks/scripts/tf_cnn_benchmarks
+script -c "python tf_cnn_benchmarks.py --num_gpus=4 --batch_size=32 --use_fp16 --model=resnet50 --variable_update=parameter_server" /workspace/logs/benchmark_resnet50_synthetic.log
+if [ -d "/tfrecords" ] ; then
+  script -c "python tf_cnn_benchmarks.py --data_format=NCHW --batch_size=32 --model=resnet50 --optimizer=momentum --variable_update=replicated --nodistortions --gradient_repacking=8 --num_gpus=4 --num_epochs=90 --weight_decay=1e-4 --data_dir=/tfrecords/ --data_name=imagenet --use_fp16 --train_dir=/workspace/resnet50_train" /workspace/logs/benchmark_resnet50_imagenet.log
+fi
+
+cd /workspace/mlperf
+if [ ! -d "/workspace/mlperf/training" ] ; then
   git clone https://github.com/mlperf/training.git
 fi
 #object detection with coco
 cd training/object_detection/
+cp /run_and_time_obj_detect.sh .
 docker pull 140.96.29.39:5000/mlperf/object_detection:latest
-if [ ! -d "pytorch/datasets/coco" ]; then
+if [ ! -d "pytorch/datasets/coco" ] ; then
   source download_dataset.sh
 fi
-script -c 'docker run --runtime nvidia -v $WORKDIR:/workspace -it --rm --ipc=host mlperf/object_detection bash -c "cd mlperf/training/object_detection && ./install.sh && ./run_and_time.sh"' /workspace/logs/mlperf_object_detection.log
+script -c 'docker run --runtime nvidia -v $WORKDIR:/workspace -it --rm --ipc=host 140.96.29.39:5000/mlperf/object_detection:latest bash -c "cd mlperf/training/object_detection && ./install.sh && bash run_and_time_obj_detect.sh"' /workspace/logs/mlperf_object_detection.log
---
+++ run_and_time_obj_detect.sh
@@ -0,0 +1,6 @@
+export WORLD_SIZE=8
+pushd pytorch
+for ((rank=0;rank<WORLD_SIZE;rank++)); do
+  time  RANK=$rank python tools/train_mlperf.py --local_rank $rank --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml"        SOLVER.IMS_PER_BATCH 2 TEST.IMS_PER_BATCH 1 SOLVER.MAX_ITER 720000 SOLVER.STEPS "(480000, 640000)" SOLVER.BASE_LR 0.0025 &
+done
+popd
