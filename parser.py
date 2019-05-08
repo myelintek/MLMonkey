8a91e7a7a32d34fa136f2e478720eb3646f20304
Ana <itriA50562@itri.org.tw>
2019-05-08 10:26:10 +0000

    test multiple batch size and gpu scalability

--- parser.py
+++ parser.py
@@ -1,9 +1,11 @@
 import re
 import csv
 import os
+from os import listdir
+from os.path import join, isfile
 LOGS_PATH = "/workspace/logs"
 pattern = re.compile("(\d*.\d*) real")
-result_time = {}
+log_groups = {}
 
 def readlines_reverse(filename):
     with open(filename) as qfile:
@@ -21,14 +23,19 @@ def readlines_reverse(filename):
             position -= 1
         yield line[::-1]
 
-files = [f for f in os.listdir(LOGS_PATH) if os.path.isfile(os.path.join(LOGS_PATH, f))]
-for filename in files:
-    i=0
-    times = []
-    for line in readlines_reverse(os.path.join(LOGS_PATH,filename)):
-        times += pattern.findall(line) 
-        i=i+1
-        if i >=10: break
-    time = max(times)
-    result_time[filename] = time
-print(result_time)
+dirs = listdir(LOGS_PATH)
+#files = [f for f in listdir(LOGS_PATH) if isfile(os.path.join(LOGS_PATH, f))]
+for d in dirs:
+    files = [f for f in listdir(join(LOGS_PATH, d)) if isfile(join(LOGS_PATH, d, f))]
+    result_time = {}
+    for filename in files:
+        i=0
+        times = []
+        for line in readlines_reverse(join(LOGS_PATH, d, filename)):
+            times += pattern.findall(line) 
+            i=i+1
+            if i >=10: break
+        if times: time = max(times)
+        result_time[filename] = time
+    log_groups[d] = result_time 
+print(log_groups)
--- run.sh
+++ run.sh
@@ -1,17 +1,43 @@
+#!/bin/bash
 cd /workspace
 mkdir -p mlperf
 mkdir -p logs
+cd logs
+mkdir -p gpu_scalability fp16 COCO
 cd /run_benchmarks/benchmarks/scripts/tf_cnn_benchmarks
-script -c "time -f '\t%e real,\t%U user,\t%S sys' python tf_cnn_benchmarks.py --num_gpus=4 --batch_size=32 --use_fp16 --model=resnet50 --variable_update=parameter_server" /workspace/logs/benchmark_resnet50_synthetic.log
+#find max batch size for fp16 and fp32
+bs1gpu=( 1024 512 256 128 64 32 16 )
+for bsize in ${bs1gpu[@]}
+do
+  python tf_cnn_benchmarks.py --num_gpus=6 --batch_size=$bsize --num_batches=10 --use_fp16 --model=resnet50 --variable_update=replicated
+  test $? -eq 0 && bsfp16=$bsize && break
+done
+
+for bsize in ${bs1gpu[@]}
+do
+  python tf_cnn_benchmarks.py --num_gpus=6 --batch_size=$bsize --num_batches=10 --model=resnet50 --variable_update=replicated
+  test $? -eq 0 && bsfp32=$bsize && break
+done
+
+#gpus scalability test
+num_gpus=( 1 2 4 6 ) #TODO change to ( 1 2 4 8 )
+
+for gpus in ${num_gpus[@]}
+do
+  script -c "time -f '\t%e real,\t%U user,\t%S sys' python tf_cnn_benchmarks.py --num_gpus=$gpus --batch_size=$bsfp32 --num_batches=10 --model=resnet50 --variable_update=replicated --all_reduce_spec=nccl" /workspace/logs/gpu_scalability/$gpus.log
+done
+
+#fp16 test #TODO change --num_gpus=8
+script -c "time -f '\t%e real,\t%U user,\t%S sys' python tf_cnn_benchmarks.py --num_gpus=6 --batch_size=$bsfp16 --num_batches=10 --model=resnet50 --variable_update=replicated" /workspace/logs/fp16/synthetic.log
 if [ -d "/tfrecords" ] ; then
-  script -c "time -f '\t%e real,\t%U user,\t%S sys' python tf_cnn_benchmarks.py --data_format=NCHW --batch_size=32 --model=resnet50 --optimizer=momentum --variable_update=replicated --nodistortions --gradient_repacking=8 --num_gpus=4 --num_epochs=90 --weight_decay=1e-4 --data_dir=/tfrecords/ --data_name=imagenet --use_fp16 --train_dir=/workspace/resnet50_train" /workspace/logs/benchmark_resnet50_imagenet.log
+  script -c "time -f '\t%e real,\t%U user,\t%S sys' python tf_cnn_benchmarks.py --data_format=NCHW --batch_size=$bsfp16 --model=resnet50 --optimizer=momentum --variable_update=replicated --nodistortions --gradient_repacking=8 --num_gpus=6 --num_epochs=90 --weight_decay=1e-4 --data_dir=/tfrecords/ --data_name=imagenet --use_fp16 --train_dir=/workspace/resnet50_train" /workspace/logs/fp16/imagenet.log
 fi
 
+#mlperf COCO	
 cd /workspace/mlperf
 if [ ! -d "/workspace/mlperf/training" ] ; then
   git clone https://github.com/mlperf/training.git
 fi
-#object detection with coco
 cd training/object_detection/
 cp /run_and_time_obj_detect.sh .
 chmod +x run_and_time_obj_detect.sh
@@ -19,4 +45,4 @@ docker pull 140.96.29.39:5000/mlperf/object_detection:latest
 if [ ! -d "pytorch/datasets/coco" ] ; then
   source download_dataset.sh
 fi
-script -c "docker run --runtime nvidia -v $WORKDIR:/workspace -e MASTER_ADDR=localhost -e MASTER_PORT=4000 -e TIMEFORMAT='%E real,  %U user,  %S sys' -it --rm --ipc=host 140.96.29.39:5000/mlperf/object_detection:latest bash -c 'cd mlperf/training/object_detection && ./install.sh && ./run_and_time_obj_detect.sh'" /workspace/logs/mlperf_object_detection.log
+script -c "docker run --runtime nvidia -v $WORKDIR:/workspace -e MASTER_ADDR=localhost -e MASTER_PORT=4000 -e TIMEFORMAT='%E real,  %U user,  %S sys' -it --rm --ipc=host 140.96.29.39:5000/mlperf/object_detection:latest bash -c 'cd mlperf/training/object_detection && ./install.sh && ./run_and_time_obj_detect.sh'" /workspace/logs/COCO/mlperf_object_detection.log
