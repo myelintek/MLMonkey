944172c863feb7866d1df2d8ec1ecbc86aa9d3b0
Ana <itriA50562@itri.org.tw>
2019-05-17 05:34:30 +0000

    increase number of itter for real test

--- scripts/run_and_time_obj_detect.sh
+++ scripts/run_and_time_obj_detect.sh
@@ -1,7 +1,7 @@
-export WORLD_SIZE=4
+export WORLD_SIZE=8
 pushd pytorch
 for ((rank=0;rank<WORLD_SIZE-1;rank++)); do
-  time  RANK=$rank python tools/train_mlperf.py --local_rank $rank --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 4 TEST.IMS_PER_BATCH 1 SOLVER.MAX_ITER 72 SOLVER.STEPS "(48, 64)" SOLVER.BASE_LR 0.0025 &
+  time  RANK=$rank python tools/train_mlperf.py --local_rank $rank --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 16 TEST.IMS_PER_BATCH 8 SOLVER.MAX_ITER 720000 SOLVER.STEPS "(480000, 640000)" SOLVER.BASE_LR 0.0025 &
 done
-time  RANK=$(($WORLD_SIZE-1)) python tools/train_mlperf.py --local_rank $(($WORLD_SIZE-1)) --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 4 TEST.IMS_PER_BATCH 1 SOLVER.MAX_ITER 72 SOLVER.STEPS "(48, 64)" SOLVER.BASE_LR 0.0025
+time  RANK=$(($WORLD_SIZE-1)) python tools/train_mlperf.py --local_rank $(($WORLD_SIZE-1)) --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 16 TEST.IMS_PER_BATCH 8 SOLVER.MAX_ITER 720000 SOLVER.STEPS "(480000, 640000)" SOLVER.BASE_LR 0.0025
 popd
